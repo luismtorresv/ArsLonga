ced121fc1501f6846479a5d311a320fe63831d2f
Luis Torres <lmtorresv@eafit.edu.co>
2025-09-14 17:47:20 -0500

    Use `__` helper function in `*.blade.php` files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
-    <title>Ars Longa</title>
+    <title>{{ __('Ars Longa') }}</title>
 </head>
 
 <body>
     <!--Header-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand ms-3" href="{{ route('home.index') }}">Ars Longa</a>
+        <a class="navbar-brand ms-3" href="{{ route('home.index') }}">{{ __('Ars Longa') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,29 +21,29 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('home.index') }}">Auction</a>
+                    <a class="nav-link active" href="{{ route('home.index') }}">{{ __('Auction') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('home.index') }}">Artwork</a>
+                    <a class="nav-link active" href="{{ route('home.index') }}">{{ __('Artwork') }}</a>
                 </li>
                 <div class="vr bg-white mx-2 d-none d-lg-block"></div>
                 @guest
-                <a class="nav-link active" href="{{ route('login') }}">Login</a>
-                <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                <a class="nav-link active" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="nav-link active" href="{{ route('register') }}">{{ __('Register') }}</a>
                 @else
                 <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('home.index') }}">Profile</a>
+                    <a class="nav-link" href="{{ route('home.index') }}">{{ __('Profile') }}</a>
                 </li>
                 <form id="logout" action="{{ route('logout') }}" method="POST">
                     <a role="button" class="nav-link active"
-                        onclick="document.getElementById('logout').submit();">Logout</a>
+                        onclick="document.getElementById('logout').submit();">{{ __('Logout') }}</a>
                     @csrf
                 </form>
                 @endguest
             </ul>
         </div>
     </nav>
-    <div class="container">Ars Longa</div>
+    <div class="container">{{ __('Ars Longa') }}</div>
 
     <div class="container my-4">
         @yield('content')
@@ -52,7 +52,7 @@
     <div class="copyright py-4 text-center text-white">
         <div class="container">
             <small>
-                Copyright — Ars Longa
+                {{ __('Copyright — Ars Longa') }}
             </small>
         </div>
     </div>
