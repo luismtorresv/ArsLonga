0f4b851d85a8856a4e616f05d0d1c747f11998e9
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-13 21:39:42 -0500

    installed laravel/ui and created js folder in resources. Added migrations. Created header with corresponding links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,42 @@
 </head>
 
 <body>
+    <!--Header-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand ms-3" href="{{ route('home.index') }}">Ars Longa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('home.index') }}"> Auction </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('home.index') }}"> Artwork </a>
+                </li>
+                <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                @guest
+                <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                @else
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('home.index') }}"> Profile </a>
+                </li>
+                <form id="logout" action="{{ route('logout') }}" method="POST">
+                    <a role="button" class="nav-link active"
+                        onclick="document.getElementById('logout').submit();">Logout</a>
+                    @csrf
+                </form>
+                @endguest
+            </ul>
+        </div>
+    </nav>
+
+
+
+
     <div class="container">Ars Longa</div>
 
     <div class="container my-4">
