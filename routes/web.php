12f082da5aab92bcd33f433071e7e70aa94b7aad
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-14 19:24:52 -0500

    Create migration and controller for `Auction`

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
+// Auction routes
+Route::get('/auction', 'App\Http\Controllers\AuctionController@index')->name('auction.index');
+
 Auth::routes();
