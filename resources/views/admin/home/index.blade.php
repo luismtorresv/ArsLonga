0a515babd03127a9d04700aeaff558c37216b967
Wendy-B11 <wendy.benig54@gmail.com>
2025-09-18 16:25:02 -0500

    Admin view

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('content')
+<div class="card"> 
+  <div class="card-header"> 
+    {{ __('Ars Longa - Admin Panel') }}
+  </div> 
+  <div class="card-body"> 
+    {{ __('Navigate!') }} 
+  </div> 
+</div> 
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index"); 
 
 Auth::routes();
