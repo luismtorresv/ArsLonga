c2a45928878858287a7e355879a2f2888629d4df
Luis Torres <lmtorresv@eafit.edu.co>
2025-09-19 13:55:24 -0500

    Localise `Artwork`-related views

---
+++ lang/en/artwork.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author Luis Torres
+ */
+
+return [
+    'title' => [
+        'index' => 'List of artworks',
+        'show' => ':artworkTitle - Artwork information',
+    ],
+    'index' => [
+        'no_artworks' => 'No artworks available in the gallery yet.',
+    ],
+    'show' => [
+        'about' => 'About this piece',
+        'artworkID' => 'Artwork ID: :artworkID',
+        'by' => 'By :author',
+    ],
+];
