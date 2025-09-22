76d9cedeecb14d8352c30d866b13d52540f1f171
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-22 18:14:10 -0500

    modify BidController and add respective views

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -6,6 +6,9 @@
 
 namespace App\Http\Controllers;
 
+use app\Models\Bid;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BidController extends Controller
@@ -13,5 +16,13 @@ class BidController extends Controller
     public function create(): View
     {
         return view('bid.create');
+    }
+
+    public function save(request $request): RedirectResponse
+    {
+        Bid::validate($request);
+        Bid::create($request->only(['price_offering', 'user_id', 'auction_id']));
+
+        return back();
     }
 }
