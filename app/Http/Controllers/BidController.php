d303e1b95b94f64265cd43c88cfa8d9c6c0d7a00
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-22 17:40:36 -0500

    add starting files for bids

---
+++ app/Http/Controllers/BidController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Jeronimo Acosta
+ */
+
+namespace App\Http\Controllers;
+use Illuminate\View\View;
+
+class BidController extends Controller
+{
+
+    public function create(): View
+    {
+        return view('bid.create');
+    }
+
+}
--- database/migrations/2025_09_22_223315_create_bids_table.php
+++ database/migrations/2025_09_22_223315_create_bids_table.php
@@ -8,19 +8,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('auctions', function (Blueprint $table): void {
+        Schema::create('bids', function (Blueprint $table) {
             $table->id();
+            $table->integer('price_offering');
             $table->timestamps();
-            $table->integer('price_limit');
-            $table->foreignId('winning_bidder_id')->nullable()->constrained('users')->nullOnDelete();
-            $table->foreignId('artwork_id')->constrained('artworks')->restrictOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
         });
     }
 
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('auctions');
+        Schema::dropIfExists('bids');
     }
 };
