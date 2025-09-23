b8f07cafa854cb84f13f42557daf5cd1453c2591
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-22 21:05:16 -0500

    make required changes

---
+++ lang/en/bid.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Jeronimo Acosta
+ */
+
+return [
+    'create' => [
+        'title' => 'Create Bid',
+        'offer_placeholder' => 'Enter your offer',
+        'submit_button' => 'Send',
+    ],
+];
