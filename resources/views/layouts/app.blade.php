9eb3a493489875086090e9fe7fdd1ee6fc3d2815
Luis Torres <lmtorresv@eafit.edu.co>
2025-10-13 22:18:07 -0500

    Use a custom font (Rubik, via Google Fonts) for the content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
+
+    {{-- Custom font for the content (see usage below). --}}
+    <link href="{{ asset('app.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
+
     <title>{{ __('Ars Longa') }}</title>
 </head>
 
 <body>
     @include('layouts.header')
 
-    <div class="container my-4">
+    <div class="container my-4 arslonga-rubik-font">
         @yield('content')
     </div>
 
