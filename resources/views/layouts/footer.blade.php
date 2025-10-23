8642c0673ba9ea9f90e9654f8d919a8d7e6e885b
Luis Torres <lmtorresv@eafit.edu.co>
2025-10-23 15:42:40 -0500

    Localise end-user layout
    Related to #52.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <footer class="py-3 my-4">
-        <p class="text-center text-body-secondary border-bottom mb-3 pb-3">{{ __('© 2025 Ars Longa') }}</p>
-        <p class="text-center text-body-tertiary fst-italic">{{ __('Ars longa, vita felix.') }}</p>
+        <p class="text-center text-body-secondary border-bottom mb-3 pb-3">{{ __('layouts.footer.copyright') }}</p>
+        <p class="text-center text-body-tertiary fst-italic">{{ __('layouts.footer.slogan') }}</p>
     </footer>
 </div>
