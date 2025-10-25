5eabd08cc0e33190763f8e7c1f96086195e72329
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-10-24 20:15:00 -0500

    Fix typos; fix `closeAuction` method; change `id` for `name` in `auction.blade.show`

--- database/migrations/2025_10_24_003422_add_final_date_to_auctions_table.php
+++ database/migrations/2025_10_24_003422_add_final_date_to_auctions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('auctions', function (Blueprint $table) {
-            $table->timestamp('final_date');
+            $table->timestamp('final_date')->nullable();
         });
     }
 
