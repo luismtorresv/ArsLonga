7a5d79a3e5f2934da113c9c2bc26a3527bf3367c
Luis Torres <lmtorresv@eafit.edu.co>
2025-10-25 11:11:34 -0500

    Translate `auth.php` into Spanish
    Related to #52.

---
+++ lang/es/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'flow' => [
+        'login' => 'Iniciar sesión',
+        'remember_me' => 'Mantén mi sesión',
+        'logout' => 'Cerrar sesión',
+        'sign_up' => 'Registrarse',
+        'password' => [
+            'new_password' => 'Nueva contraseña',
+            'change_password' => 'Cambiar contraseña',
+            'confirm_password' => 'Confirmar contraseña',
+            'please_confirm_password' => 'Por favor confirma tu contraseña antes de continuar.',
+            'forgot_password' => '¿Olvidaste tu contraseña?',
+            'reset_password' => 'Restablecer contraseña',
+            'send_password_reset_link' => 'Mandar enlace para restablecer contraseña',
+        ],
+        'email_verification' => [
+            'verify_email' => 'Verificar dirección de correo electrónico',
+            'check_email' => 'Antes de proceder, por favor verifica el enlace de verificación que se envió a tu correo electrónico',
+            'did_not_receive_email' => 'Si no recibiste el correo electrónico',
+            'verification_link_sent' => 'Un nuevo enlace verficación se envió a tu correo electrónico',
+            'request_new_link' => 'Solicitar un nuevo enlace',
+        ],
+    ],
+    'alerts' => [
+        'profile' => [
+            'updated' => 'Perfil actualizado con éxito',
+        ],
+        'password' => [
+            'updated' => 'Contraseña actualizada con éxito',
+        ],
+    ],
+    'name' => 'Nombre',
+    'email_address' => 'Dirección de correo electrónico',
+    'password' => 'Contraseña',
+    'profile' => 'Perfil',
+    'failed' => 'Estos credenciales no coinciden con nuestros registros',
+    'incorrect_password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de iniciar sesión. Por favor intenta de nuevo en :seconds segundos.',
+
+];
