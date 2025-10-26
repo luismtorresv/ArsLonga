62364e4387c741f5e12db991939db6758f3caa0c
Luis Torres <lmtorresv@eafit.edu.co>
2025-10-26 17:21:29 -0500

    Add `AuctionFactory` for `DatabaseSeeder`

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -7,6 +7,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -14,6 +15,8 @@ use Illuminate\Http\Request;
 
 class Auction extends Model
 {
+    use HasFactory;
+
     /**
      * AUCTION ATTRIBUTES
      * $this->attributes['id'] - int - contains the auction's primary key (id)
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Artwork;
+use App\Models\Auction;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Artwork::factory(5)->create();
+
+        Auction::factory(5)->create(); // This will create more Artworks.
     }
 }
