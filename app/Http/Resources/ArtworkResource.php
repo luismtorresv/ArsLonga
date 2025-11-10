eccc229518b401c103bc1504883fc88ad092fd04
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-09 22:53:03 -0500

    Create `ArtworkResource` to modify API response

---
+++ app/Http/Resources/ArtworkResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArtworkResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->getId(),
+            'title' => $this->getTitle(),
+            'author' => $this->getAuthor(),
+            'price' => $this->getPrice(),
+            'image' => asset($this->getImage()),
+            'details' => $this->getDetails(),
+            'keyword' => $this->getKeyword(),
+            'category' => $this->getCategory(),
+        ];
+    }
+}
