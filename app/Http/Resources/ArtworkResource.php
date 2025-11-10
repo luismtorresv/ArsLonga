6513d722fd2bd87442b02cc56a686f38d049f68d
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-10 07:51:35 -0500

    Add details to response fields of the `ArtworkResource`

--- app/Http/Resources/ArtworkResource.php
+++ app/Http/Resources/ArtworkResource.php
@@ -10,14 +10,49 @@ class ArtworkResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            /**
+             * Unique ID of the artwork.
+             */
             'id' => $this->getId(),
+
+            /**
+             * Title of the artwork.
+             */
             'title' => $this->getTitle(),
+
+            /**
+             * Author of the artwork.
+             */
             'author' => $this->getAuthor(),
+
+            /**
+             * Price of the artwork. Always in US dollars.
+             */
             'price' => $this->getPrice(),
-            'image' => asset('/storage/'.$this->getImage()),
+
+            /**
+             * Additional details related to the artwork.
+             */
             'details' => $this->getDetails(),
+
+            /**
+             * Short keyword like 'oil' or 'impressionism'.
+             */
             'keyword' => $this->getKeyword(),
+
+            /**
+             * Type of artwork: Painting, Sculpture, etc.
+             */
             'category' => $this->getCategory(),
+
+            /**
+             * Link to the image associated to the artwork.
+             */
+            'image' => asset('/storage/'.$this->getImage()),
+
+            /**
+             * Direct link to the artwork's page in the store.
+             */
             'link' => route('artwork.show', ['id' => $this->getId()]),
         ];
     }
