30802038a916187de836a3f010fd7183b628c41d
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-10 12:49:42 -0500

    Use simple titles and authors in `ArtworkFactory`

--- database/factories/ArtworkFactory.php
+++ database/factories/ArtworkFactory.php
@@ -30,27 +30,8 @@ class ArtworkFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->randomElement([
-                'Salvator Mundi', 'Interchange', 'The Card Players',
-                'Nafea Faa Ipoipo (When Will You Marry?)', 'Number 17A',
-                'Wasserschlangen II', 'No. 6 (Violet, Green and Red)',
-                'Pendant portraits of Maerten Soolmans and Oopjen Coppit',
-                'Les Femmes d\'Alger ("Version O")', 'The Standard Bearer',
-                'Shot Sage Blue Marilyn', 'Nu couché', 'No. 5, 1948',
-                'Woman III', 'Masterpiece', 'Portrait of Adele Bloch-Bauer I',
-                'Le Rêve', 'Portrait of Dr. Gachet',
-                'Portrait of Adele Bloch-Bauer II',
-            ]),
-            'author' => $this->faker->randomElement([
-                'Peter Agostini', 'Lucien den Arend', 'Karl Bitter',
-                'Percival Ball', 'Michael Boroniec', 'Hans Van de Bokenvamp',
-                'Jacob Burck', 'Estella Campavias', 'Nancy Carline',
-                'Bartolomeo Cavaceppi', 'Thomas Hart Benton', 'Fernando Botero',
-                'Clarence Holbrook Carter', 'Charles Conder', 'Sonia Delaunay',
-                'Elizabeth Durack', 'M. C. Escher', 'Joaquín Torres García',
-                'Natalia Goncharova', 'Nina Genke-Meller', 'Elaine Hamilton',
-                'Friedensreich Hundertwasser', 'František Kupka', 'László Moholy-Nagy',
-            ]),
+            'title' => $this->faker->text(maxNbChars: 40),
+            'author' => $this->faker->name(),
             'keyword' => $this->faker->word(),
             'category' => $this->faker->randomElement([
                 'Painting', 'Sculpture', 'Photography', 'Digital', 'Drawing',
