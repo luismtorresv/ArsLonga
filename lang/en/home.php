7a69b766c4c098fe3bfc87600ab1d58fa5ddd920
Wendy-B11 <wendy.benig54@gmail.com>
2025-11-10 14:26:19 -0500

    Some changes

---
+++ lang/en/home.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => 'Welcome!',
+    'curious_message' => 'Curious on what you need to commit the perfect crime? See the products below... Or just go buy an artwork or bid on an auction.',
+    'featured_products' => 'Featured Products',
+];
