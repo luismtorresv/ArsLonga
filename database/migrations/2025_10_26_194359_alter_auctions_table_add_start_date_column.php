cd20fe78802a01f584d9741592eba5b587f846bf
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-11 00:46:15 -0500

    Fix table name of rollback function in migration

--- database/migrations/2025_10_26_194359_alter_auctions_table_add_start_date_column.php
+++ database/migrations/2025_10_26_194359_alter_auctions_table_add_start_date_column.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('auctions', function (Blueprint $table) {
             $table->dropColumn('start_date');
         });
     }
