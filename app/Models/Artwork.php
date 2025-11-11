cbf7fe5211eeb14ba8d44035cf51909ed9739957
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-11 00:48:34 -0500

    Add missing Eloquent relationships to `Artwork`
    One of the pending corrections from last deliverable.
    
    Closes #43.

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -165,5 +166,15 @@ class Artwork extends Model
     public function getUpdatedAt(): string
     {
         return $this->attributes['updated_at'];
+    }
+
+    public function auction(): BelongsTo
+    {
+        return $this->belongsTo(Auction::class);
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
     }
 }
