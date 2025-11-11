9d8c170e09b70a9acad8fd4e4f292ed92e46fcce
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-10 23:15:28 -0500

    Add missing property docstring to `Item`

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -18,6 +18,7 @@ use Illuminate\Http\Request;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\Artwork $artwork
+ * @property-read \App\Models\Order $order
  *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Item newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Item newQuery()
